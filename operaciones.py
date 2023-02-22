81d6a12b15d6f5b82ae6f7a00440b05c10b318c3
JoseGutierrez <jagutierrez830@misena.edu.co>
2023-02-22 18:16:25 -0500

    Script js operaciones

---
+++ operaciones.py
@@ -0,0 +1,17 @@
+# 2 numeros 4 operaciones
+
+print("Ingrese un número: ")
+num1= float(input())
+Print("ingrese un segundo número: ")
+num2= float(input())
+
+suma= num1+num2
+resta= num1-num2
+multi= num1*num2
+div= num1/num2
+print("La suma es: ", suma)
+print("La resta es: ", resta)
+print("La multiplicacion es: ", multi)
+print("La divicion es: ", div)
+
+
